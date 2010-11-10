196d86788fe01b3c77c36029ff7c08680a4fdcd3
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2010-11-10 00:59:17 +0000

    Fixed cleanup script.

--- trunk/source/clean.cmd
+++ trunk/source/clean.cmd
@@ -4,4 +4,4 @@ for /R %%i in (*.aps) do del "%%i"
 for /R %%i in (*.ncb) do del "%%i"
 attrib -H *.suo /S
 for /R %%i in (*.suo) do del "%%i"
-for /R %%i in ("*.vcproj.%COMPUTERNAME%.%USERNAME%.user") do del "%%i"
+for /R %%i in ("*.vcproj.%USERDOMAIN%.%USERNAME%.user") do del "%%i"
