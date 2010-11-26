c5ea427393868d6b555f16a57fc002bd4bb78a84
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2010-11-26 12:00:49 +0000

    bCPropertyConverter finished.

--- trunk/source/SharedBase/ge_propertyconverter.h
+++ trunk/source/SharedBase/ge_propertyconverter.h
@@ -3,10 +3,13 @@
 
 class bCObjectBase;  // forward
 
+//TODO: bCPropertyConverter - How to create array elements?
+//TODO: bCPropertyConverter - How to delete array elements?
+
 class GE_DLLIMPORT bCPropertyConverter
 {
-public: virtual GEU32                  AddReference( void );                         // [0000].0000
-public: virtual GEU32                  ReleaseReference( void );                     // [0000].0004
+public: virtual GEU32                  AddReference( void );                          // [0000].0000
+public: virtual GEU32                  ReleaseReference( void );                      // [0000].0004
 public: virtual bCString const &       GetPropertyName( void ) const            = 0;  // [0000].0008
 public: virtual GELPCChar              GetClassName( void ) const               = 0;  // [0000].000C
 public: virtual GELPCChar              GetValueTypeName( void ) const           = 0;  // [0000].0010
@@ -22,7 +25,7 @@ public: virtual GEInt                  GetElementCount( void )                  
 public: virtual bCPropertyConverterPtr GetElement( GEInt )                      = 0;  // [0000].0038
 public: virtual GEBool                 CopyFrom( bCPropertyConverterPtr )       = 0;  // [0000].003C
 public: virtual GEBool                 IsEqual( bCPropertyConverterPtr )        = 0;  // [0000].0040
-public: virtual GEBool                 IsElement( void )                        = 0;  // [0000].0044
+public: virtual GEBool                 IsArrayElement( void )                   = 0;  // [0000].0044
 public: virtual GEBool                 IsArray( void )                          = 0;  // [0000].0048
 public: virtual GEBool                 IsNonArray( void )                       = 0;  // [0000].004C
 public: virtual bCPropertyConverterPtr GetArray( void )                         = 0;  // [0000].0050
@@ -31,8 +34,8 @@ public: virtual GEBool                 Contains( bCPropertyConverterPtr )       
 public: virtual bCObjectBase *         GetPropertyClass( void )                 = 0;  // [0000].005C
 public: virtual                       ~bCPropertyConverter( void );                   // [0000].0060
 // bTPropertyConverterTyped< TYPE >
-// public: virtual T &                 PropertyGetter( void )                   = 0;  // [0000].0064
-// public: virtual void                SetValue( T const & )                    = 0;  // [0000].0068
+// public: virtual TYPE const &        PropertyGetter( void )                   = 0;  // [0000].0064
+// public: virtual void                SetValue( TYPE const & )                 = 0;  // [0000].0068
 private:
     GEU32 m_u32ReferenceCount;  // 0004
     // sizeof(bCPropertyConverter) 0008
