584c798b8022a6b1c7830bafebc90f3496fb9e66
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-01-27 13:28:04 +0000

    TILib I/O stream base classes.

--- source/SharedBase/ge_iostream.h
+++ source/SharedBase/ge_iostream.h
@@ -1,12 +1,22 @@
 #ifndef GE_IOSTREAM_H_INCLUDED
 #define GE_IOSTREAM_H_INCLUDED
 
+enum bEStreamSeekMode
+{
+    bEStreamSeekMode_Begin,
+    bEStreamSeekMode_Current,
+    bEStreamSeekMode_End,
+    bEStreamSeekMode_ForceDWORD = GE_FORCE_DWORD
+};
+
 class GE_DLLIMPORT bCIOStream :
     public bCIStream,
     public bCOStream
 {
 public: using bCIStream::Read;
 public: virtual         ~bCIOStream( void );
+public: virtual GEI64    GetPosition( void ) const = 0;
+public: virtual GEBool   Seek( GEI64, bEStreamSeekMode ) = 0;
 public: using bCOStream::Write;
 public:
     bCIOStream & operator = ( bCIOStream const & );
