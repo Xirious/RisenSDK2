4e492b414012d19599c0dfe02d0ca1d22fd52124
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-01-27 13:28:04 +0000

    TILib I/O stream base classes.

--- trunk/source/SharedBase/ge_file.h
+++ trunk/source/SharedBase/ge_file.h
@@ -9,28 +9,20 @@ enum bEFileCreationMode
     bEFileCreationMode_ForceDWORD = GE_FORCE_DWORD
 };
 
-enum bEStreamSeekMode
-{
-    bEStreamSeekMode_Begin,
-    bEStreamSeekMode_Current,
-    bEStreamSeekMode_End,
-    bEStreamSeekMode_ForceDWORD = GE_FORCE_DWORD
-};
-
 class GE_DLLIMPORT bCFile :
     public bCIOStream
 {
-public: using bCIStream::Read;
-public: virtual         ~bCFile( void );
-public: virtual GEI64    GetPosition( void ) const;
-public: virtual GEBool   Seek( GEI64, bEStreamSeekMode ) = 0;
-public: virtual GEBool   IsValid( void ) const;
-public: virtual GEBool   IsOpen( void ) const;
-public: virtual GEI64    GetLength( void ) const;
-public: virtual GEBool   Open( bCString const &, bEFileCreationMode );
-public: virtual GEBool   Close( void );
-public: virtual GEBool   Seek( GEI64, bEStreamSeekMode ) const;
-public: using bCOStream::Write;
+public: using  bCIStream::Read;
+public: virtual          ~bCFile( void );
+public: virtual GEI64     GetPosition( void ) const;
+public: using bCIOStream::Seek;
+public: virtual GEBool    IsValid( void ) const;
+public: virtual GEBool    IsOpen( void ) const;
+public: virtual GEI64     GetLength( void ) const;
+public: virtual GEBool    Open( bCString const &, bEFileCreationMode );
+public: virtual GEBool    Close( void );
+public: virtual GEBool    Seek( GEI64, bEStreamSeekMode ) const;
+public: using  bCOStream::Write;
 public:
     GEBool Eof( void ) const;
     GEBool SeekToBegin( void ) const;
