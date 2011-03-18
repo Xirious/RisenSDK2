9425b1fa0eac4173664b57c52c1f8ad04adcb130
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-03-18 09:12:36 +0000

    Multiple script patch handling added.

--- trunk/source/Scripts/Script_Mod_Fixes/Script_Mod_Fixes.cpp
+++ trunk/source/Scripts/Script_Mod_Fixes/Script_Mod_Fixes.cpp
@@ -1,14 +1,21 @@
 #include "Script/Story/Quest/Chapter1.h"
 
-void AddScript( gSScriptInit & a_ScriptInit, GELPCChar a_pcName, GELPCChar a_pcSource, gFScript a_funcScript )
+void AddScriptPatch( gSScriptInit & a_ScriptInit, GELPCChar a_pcOriginalFileName, GELPCChar a_pcName, GELPCChar a_pcSource, gFScript a_funcScript )
 {
     eCScriptFunctionMap & ScriptFunctionMap = eCScriptFunctionMap::GetInstance();
     eSScriptBase * pScript = const_cast< eSScriptBase * >( ScriptFunctionMap.GetScript( a_pcName ) );
     if( pScript && pScript->m_funcFunction )
     {
-        pScript->m_funcFunction = 0;
+        gSScriptDLL * pScriptDLL = static_cast< gSScriptDLL * >( pScript->m_pScriptDLL );
+        if( !pScriptDLL ||
+            !pScriptDLL->m_strFileName.CompareNoCase( a_pcOriginalFileName ) ||
+            g_MessageBox( 0, bCString::GetFormattedString(
+              "The script \"%s\" is already overridden by the \"%s\"!\n"
+              "Do you want to apply the patch anyway?",
+              a_pcName, pScriptDLL->m_strFileName ),
+              "Script Modification - Fixes", MB_ICONEXCLAMATION | MB_YESNO ) == IDYES )
         {
-            gSScriptDLL * pScriptDLL = static_cast< gSScriptDLL * >( pScript->m_pScriptDLL );
+            pScript->m_funcFunction = 0;
             if( pScriptDLL && pScriptDLL->m_arrScripts.RemoveAt( pScriptDLL->m_arrScripts.IndexOf( pScript ) ) )
                 ScriptFunctionMap.UnRegisterScript( pScript );
         }
@@ -30,7 +37,7 @@ extern "C" __declspec( dllexport ) gSScriptInit const * GE_STDCALL ScriptInit( v
     s_ScriptInit.m_arrScriptAICallbacks.Clear();
     s_ScriptInit.m_arrScripts.Clear();
 
-    AddScript( s_ScriptInit, "OnQuestSuccess_CloseQuestWine", "Script/Story/Quest/Chapter1.cpp", OnQuestSuccess_CloseQuestWine );
+    AddScriptPatch( s_ScriptInit, "Script_Game.dll", "OnQuestSuccess_CloseQuestWine", "Script/Story/Quest/Chapter1.cpp", OnQuestSuccess_CloseQuestWine );
     
     return &s_ScriptInit;
 }
