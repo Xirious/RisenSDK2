59b7c93da571ae94b57e698d5afe8832274eab61
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-03-20 23:05:27 +0000

    Script class World.

---
+++ source/Script/gs_world.h
@@ -0,0 +1,63 @@
+#ifndef GS_WORLD
+#define GS_WORLD
+
+class GE_DLLIMPORT World
+{
+protected:
+    void * __FIXME;
+public:
+    void            AutoSave( bCString const & );
+    void            CacheEntities( void );
+    GEBool          DeleteSaveGame( bCUnicodeString const & );
+    GEBool          EnableSectorFolder( bCString const &, GEBool );
+    void            EndGame( void );
+    void            ExitGame( void );
+    bCString        FindNextSector( bCString const &, bCString const & );
+    bCString        FindNextTemplate( bCString const &, bCString const &, eEPropertySetType ) const;
+    bCString        GetCommandLineA( void );
+    Entity          GetFocus( void ) const;
+    Entity          GetInteract( void ) const;
+    Entity          GetPlayer( void ) const;
+    GEInt           GetSaveGameCount( void ) const;
+    bCString        GetSaveGameDate( bCString const & ) const;
+    bCString        GetSaveGameDateShort( bCString const & ) const;
+    bCString        GetSaveGameFileName( GEInt ) const;
+    bCUnicodeString GetSaveGameLocation( bCString const & ) const;
+    bCUnicodeString GetSaveGameName( bCString const & ) const;
+    bCUnicodeString GetSaveGamePlayTime( bCString const & ) const;
+    bCString        GetSaveGameTime( bCString const & ) const;
+    GEBool          GetSectorStatus( bCString const & );
+    Entity          GetTrader( void ) const;
+    GEBool          IsGameRunning( void ) const;
+    GEBool          IsInTestMode( void ) const;
+    GEBool          IsPaused( void ) const;
+    GEBool          IsPrefetching( void );
+    GEBool          IsSaveAllowed( void ) const;
+    void            LoadGame( bCUnicodeString const & );
+    void            NewGame( void );
+    void            Pause( void );
+    void            Prefetch( bCVector const & );
+    void            Prefetch( void );
+    void            QuickLoad( void );
+    void            QuickSave( void );
+    void            RefreshSaveGameImage( bCString const & );
+    void            RefreshSaveGameList( void );
+    void            Resume( void );
+    void            SaveConfiguration( void );
+    void            SaveGame( bCUnicodeString const & );
+    GEBool          SaveGameExists( bCString const & ) const;
+    void            SaveStatistics( void );
+    GEBool          SetSectorStatus( bCString const &, GEBool );
+    void            TogglePause( void );
+    void            UpdateScreenShot( void );
+public:
+    World & operator = ( World const & );
+public:
+    World( void );
+   ~World( void );
+};
+GE_ASSERT_SIZEOF( World, 0x0004 )
+
+GE_C_LINKAGE GE_DLLIMPORT World world;
+
+#endif
