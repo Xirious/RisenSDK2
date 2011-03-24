ca03823de167096541bb6f0d4d29756901a5e4b9
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-03-24 19:05:05 +0000

    Script_Mod_Fixes GiveXP changed and OnQuestChange added.

--- trunk/source/Scripts/Script_Mod_Fixes/Script/Story/Quest/Chapter1.h
+++ trunk/source/Scripts/Script_Mod_Fixes/Script/Story/Quest/Chapter1.h
@@ -1,6 +1,6 @@
 #ifndef CHAPTER1_H_INCLUDED
 #define CHAPTER1_H_INCLUDED
 
-GEInt GE_STDCALL OnQuestSuccess_CloseQuestWine( gCScriptProcessingUnit *, GELPVoid, GELPVoid, GEInt );
+GS_DLLEXPORT GEInt GE_STDCALL OnQuestSuccess_CloseQuestWine( gCScriptProcessingUnit *, GELPVoid, GELPVoid, GEInt );
 
 #endif
--- trunk/source/Scripts/Script_Mod_Fixes/Script_Mod_Fixes.h
+++ trunk/source/Scripts/Script_Mod_Fixes/Script_Mod_Fixes.h
@@ -2,7 +2,11 @@
 #define SCRIPT_MOD_FIXES_H_INCLUDED
 
 #include "Script.h"
+
+#define GS_DLLEXPORT __declspec( dllexport )
+
 #include "Script/Ai/AI_Commands/GiveXP.h"
 #include "Script/Story/Quest/Chapter1.h"
+#include "Script/Story/Quest/OnQuestChange.h"
 
 #endif
