c9cc7e0ba189dcbb3b211f635e1d20ce88ef55ec
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-04-06 21:15:49 +0000

    Script_Mod_Fixes changed to Script_RQMS to resolve conflicts with the Script_Game_Patch project.

--- source/Scripts/Script_Mod_Fixes/Script/Story/Quest/Chapter1.h
+++
@@ -1,6 +0,0 @@
-#ifndef CHAPTER1_H_INCLUDED
-#define CHAPTER1_H_INCLUDED
-
-GS_DLLEXPORT GEInt GE_STDCALL OnQuestSuccess_CloseQuestWine( gCScriptProcessingUnit *, GELPVoid, GELPVoid, GEInt );
-
-#endif
