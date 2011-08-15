9c1878084d1ac2766535dbdf7d67005764835ef2
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-08-15 15:04:34 +0000

    Importer classes added.

--- source/Importer.h
+++ source/Importer.h
@@ -3,8 +3,16 @@
 
 #include "Game.h"
 
-#include "Importer/ge_scenecompiler.h"
-#include "Importer/ge_resourcecompiler.h"
+class iCBaseFileReader{};
 #include "Importer/ge_democompiler.h"
+class iCMasterWriter{};
+class iCOptimizer{};
+class iCStrippifier{};
+#include "Importer/ge_importeradmin.h"
+#include "Importer/ge_meshimporter.h"
+#include "Importer/ge_resourcecompiler.h"
+#include "Importer/ge_scenecompiler.h"
+class eCMeshResource2{};
+#include "Importer/ge_texturebaker.h"
 
 #endif
