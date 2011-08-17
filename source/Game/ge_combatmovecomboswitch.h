4d3fe2339ccd44ac52539ff85d816e386cc76383
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-08-17 08:24:45 +0000

    gCCombatMoveComboSwitch added.

---
+++ source/Game/ge_combatmovecomboswitch.h
@@ -0,0 +1,51 @@
+#ifndef GE_COMBATMOVECOMBOSWITCH_H_INCLUDED
+#define GE_COMBATMOVECOMBOSWITCH_H_INCLUDED
+
+class gCCombatMoveSwitch :
+    public bCObjectBase
+{
+public:    virtual bCPropertyObjectTypeBase * GetObjectType( void ) const;
+public:    virtual                           ~gCCombatMoveSwitch( void );
+public:
+    GEFloat  m_fTime;
+    bCString m_strMove;
+};
+GE_ASSERT_SIZEOF( gCCombatMoveSwitch, 0x000C )
+
+#pragma warning( push )
+#pragma warning( disable : 4251 )  // class 'bTObjArray<T>' needs to have dll-interface to be used by clients of class 'gCCombatMoveComboSwitch'
+
+class GE_DLLIMPORT gCCombatMoveComboSwitch :
+    public gCCombatMove
+{
+public:    virtual bCPropertyObjectTypeBase * GetObjectType( void ) const;
+public:    virtual bEResult                   Create( void );
+protected: virtual void                       Destroy( void );
+public:    virtual bEResult                   PostInitializeProperties( void );
+public:    virtual                           ~gCCombatMoveComboSwitch( void );
+public:    virtual gSCombatMoveInstance *     Start( gCCombatSystem_PS *, gCCombatStyle *, gSComboData * );
+private:
+    static bCPropertyObjectTypeBase thisType;
+protected:
+    bTObjArray< gCCombatMoveSwitch > m_arrComboMoves;
+protected:
+    void Invalidate( void );
+public:
+    static bCObjectBase *             GE_STDCALL CreateObject( void );
+    static bCPropertyObjectTypeBase & GE_STDCALL GetThisType( void );
+    static void                       GE_STDCALL StaticConstructor( bCPropertyObjectTypeBase & );
+public:
+    bTObjArray< gCCombatMoveSwitch > &       AccessComboMoves( void );
+    bTObjArray< gCCombatMoveSwitch > const & GetComboMoves( void ) const;
+    void                                     SetComboMoves( bTObjArray< gCCombatMoveSwitch > const & );
+public:
+    gCCombatMoveComboSwitch & operator = ( gCCombatMoveComboSwitch const & );
+public:
+    gCCombatMoveComboSwitch( gCCombatMoveComboSwitch const & );
+    gCCombatMoveComboSwitch( void );
+};
+GE_ASSERT_SIZEOF( gCCombatMoveComboSwitch, 0x0020 )
+
+#pragma warning( pop )
+
+#endif
