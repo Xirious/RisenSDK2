459a083224dbc72fd54a8aaca3d85f6fff35942a
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-08-22 21:50:51 +0000

    Script Music class added.

---
+++ source/Script/gs_music.h
@@ -0,0 +1,23 @@
+#ifndef GS_MUSIC_H_INCLUDED
+#define GS_MUSIC_H_INCLUDED
+
+class GE_DLLIMPORT Music
+{
+public:
+    void    EnableAutoUpdate( GEBool );
+    GEFloat GetMusicVolume( void );
+    void    Request( bCString, GEInt, GEFloat );
+    void    SetMusicVolume( GEFloat );
+    void    StartOverlay( bCString );
+    void    Stop( void );
+    void    StopOverlay( void );
+public:
+    Music & operator = ( Music const & );
+public:
+    Music( void );
+   ~Music( void );
+};
+
+GE_C_LINKAGE GE_DLLIMPORT Music music;
+
+#endif
