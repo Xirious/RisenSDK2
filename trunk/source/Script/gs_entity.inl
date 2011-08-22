bec71a583c600c3bacdfdb34f5dd5921f064db7b
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-08-22 21:09:59 +0000

    Moved Entity out of PropertySet and Property scope.

--- trunk/source/Script/gs_entity.inl
+++ trunk/source/Script/gs_entity.inl
@@ -1,26 +1,14 @@
 #include "../Script.h"
 
 template< typename T >
-T & Entity::Property( void )
-{
-    return *static_cast< T::_tProperty * >( this );
-}
-
-template< typename T >
-T const & Entity::Property( void ) const
-{
-    return *static_cast< T::_tProperty const * >( this );
-}
-
-template< typename T >
 T & Entity::PropertySet( void )
 {
-    return *static_cast< T::_tPropertySet * >( this );
+    return *reinterpret_cast< T::_tPropertySet * >( this );
 }
 
 template< typename T >
 T const & Entity::PropertySet( void ) const
 {
-    return *static_cast< T::_tPropertySet const * >( this );
+    return *reinterpret_cast< T::_tPropertySet const * >( this );
 }
 
--- trunk/source/Script/gs_propertymacros.h
+++ trunk/source/Script/gs_propertymacros.h
@@ -1,40 +1,71 @@
 #ifndef GS_PROPERTYMACROS_H_INCLUDED
 #define GS_PROPERTYMACROS_H_INCLUDED
 
-#define GS_DECLARE_PROPERTY( NAME, TYPE )                            \
-class Property##NAME :                                               \
-    protected Entity                                                 \
-{                                                                    \
-protected:                                                           \
-    typedef Property##NAME _tProperty;                               \
-    friend Property##NAME & Entity::Property( void );                \
-    friend Property##NAME const & Entity::Property( void ) const;    \
-public:                                                              \
-                     operator TYPE const ( void ) const;             \
-    Property##NAME & operator =          ( TYPE const & );           \
-private:                                                             \
-    /* operator is exported but the implementation does nothing */   \
-    Property##NAME & operator = ( Property##NAME const & );          \
-private:                                                             \
-    Property##NAME( Property##NAME const & );                        \
-    Property##NAME( void );                                          \
-   ~Property##NAME( void );                                          \
+class GE_DLLIMPORT EntityProperty
+{
+protected:
+    eCEntity * m_pEngineEntity;
+private:
+    EntityProperty & operator = ( EntityProperty const & );
+private:
+    EntityProperty( EntityProperty const & );
+    EntityProperty( void );
+   ~EntityProperty( void );
+};
+
+#define GS_DECLARE_PROPERTY( NAME, TYPE )                                    \
+class Property##NAME :                                                       \
+    public EntityProperty                                                    \
+{                                                                            \
+protected:                                                                   \
+    typedef Property##NAME _tProperty;                                       \
+    friend Property##NAME & EntityPropertySet::Property( void );             \
+    friend Property##NAME const & EntityPropertySet::Property( void ) const; \
+public:                                                                      \
+                     operator TYPE const ( void ) const;                     \
+    Property##NAME & operator =          ( TYPE const & );                   \
+private:                                                                     \
+    /* operator is exported but the implementation does nothing */           \
+    Property##NAME & operator = ( Property##NAME const & );                  \
+private:                                                                     \
+    Property##NAME( Property##NAME const & );                                \
+    Property##NAME( void );                                                  \
+   ~Property##NAME( void );                                                  \
+};
+
+class GE_DLLIMPORT EntityPropertySet
+{
+protected:
+    eCEntity * m_pEngineEntity;
+public:
+    template< typename T >
+    inline T & Property( void );
+    template< typename T >
+    inline T const & Property( void ) const;
+private:
+    EntityPropertySet & operator = ( EntityPropertySet const & );
+private:
+    EntityPropertySet( EntityPropertySet const & );
+    EntityPropertySet( void );
+   ~EntityPropertySet( void );
 };
 
 #define GS_DECLARE_PROPERTYSET( CLASS )                     \
 class GE_DLLIMPORT CLASS :                                  \
-    protected Entity                                        \
+    public EntityPropertySet                                \
 {                                                           \
 protected:                                                  \
     typedef CLASS _tPropertySet;                            \
     friend CLASS & Entity::PropertySet( void );             \
     friend CLASS const & Entity::PropertySet( void ) const; \
 private:                                                    \
-    /* operator is exported but copies only one byte */     \
+    /* operator is exported but only one byte is copied */  \
     CLASS & operator = ( CLASS const & );                   \
 private:                                                    \
     CLASS( CLASS const & );                                 \
     CLASS( void );                                          \
    ~CLASS( void );
+
+#include "gs_propertymacros.inl"
 
 #endif
