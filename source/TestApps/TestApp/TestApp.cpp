f94696288993fe977fa3ad03ee64d676505aeea2
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-09-01 12:30:31 +0000

    Engine Assembly Dependency moved into a separate header.

--- source/TestApps/TestApp/TestApp.cpp
+++ source/TestApps/TestApp/TestApp.cpp
@@ -1,14 +1,5 @@
 #include "TestApp.h"
-
-#ifdef _OPENMP
-#ifdef _OPENMP_NOFORCE_MANIFEST
-#pragma comment( linker, "/manifestdependency:\"type='win32' name='Microsoft.VC80.OpenMP' version='8.0.50727.762' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"" )
-#else
-#error Please verify that the OpenMP version matches Risen’s Engine.dll
-#endif
-#else
-#pragma comment( linker, "/manifestdependency:\"type='win32' name='Microsoft.VC80.OpenMP' version='8.0.50727.762' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"" )
-#endif
+#include "EngineManifestDependency.h"
 
 static int __stdcall ProtoTypeMain( HINSTANCE a_hInstance, bCString const & a_strCmdLine, bCCommandLine const & a_CommandLine )
 {
