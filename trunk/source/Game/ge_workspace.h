4f87e1baf9add67c35144fb2f2a583f1823798b5
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-09-05 14:31:43 +0000

    gCWorkspace review.

--- trunk/source/Game/ge_workspace.h
+++ trunk/source/Game/ge_workspace.h
@@ -4,45 +4,34 @@
 class GE_DLLIMPORT gCWorkspace :
     public eCEngineComponentBase
 {
-public: virtual bCPropertyObjectTypeBase * GetObjectType( void ) const;
-public: using       eCEngineComponentBase::Create;
-public: virtual void                       Destroy( void );
-public: virtual                           ~gCWorkspace( void );
-public: virtual bEResult                   PreShutdown( void );
-private:
-    static bCPropertyObjectTypeBase thisType;
+GE_DECLARE_PROPERTY_OBJECT( gCWorkspace, eCEngineComponentBase )
+public:    virtual void                 Destroy( void );
+public:    virtual bEResult             PreShutdown( void );
+protected: using eCEngineComponentBase::Create;
 protected:
-    gCProject *  m_pCurrentProject;
-    gCDatabase * m_pGetCurrentDatabase;
-    bCString     m_FIXME;
-protected:
-    void Invalidate( void );
+    gCProject *  m_pProject;
+    gCDatabase * m_pDatabase;
+    bCString     m_strReserved;
 public:
-    static bCObjectBase *             GE_STDCALL CreateObject( void );
-    static gCWorkspace &              GE_STDCALL GetInstance( void );
-    static bCPropertyObjectTypeBase & GE_STDCALL GetThisType( void );
-    static void                       GE_STDCALL StaticConstructor( bCPropertyObjectTypeBase & );
+    static gCWorkspace & GE_STDCALL GetInstance( void );
 public:
     void               Activate( void );
-    bEResult           Create( bCString const & );
+    bEResult           Create( bCString const & a_strReserved );
     gCDatabase *       GetCurrentDatabase( void );
     gCProject *        GetCurrentProject( void );
-    gCDatabase *       GetDatabase( bCString const & );
-    gCDatabase const * GetDatabase( bCString const & ) const;
-    gCProject *        GetProject( bCString const & );
-    gCProject const *  GetProject( bCString const & ) const;
-    bEResult           Import( bCString const & );
-    gCProject *        ImportProject( eCArchiveFile & );
-    gCProject *        ImportProject( bCString const & );
-    gCDatabase *       InsertNewDatabase( bCString const & );
-    gCProject *        InsertNewProject( bCString const &, GEBool );
+    gCDatabase *       GetDatabase( bCString const & a_strReserved );
+    gCDatabase const * GetDatabase( bCString const & a_strReserved ) const;
+    gCProject *        GetProject( bCString const & a_strReserved );
+    gCProject const *  GetProject( bCString const & a_strReserved ) const;
+    bEResult           Import( bCString const & a_strReserved );
+    gCProject *        ImportProject( bCString const & a_strFileBaseName );
+    gCProject *        ImportProject( eCArchiveFile & a_File );
+    gCDatabase *       InsertNewDatabase( bCString const & a_strReserved );
+    gCProject *        InsertNewProject( bCString const & a_strFilePath, GEBool a_bIsPersistable );
     GEBool             IsDataAvailable( void ) const;
     void               RemoveAll( void );
-public:
-    gCWorkspace & operator = ( gCWorkspace const & );
-public:
-    gCWorkspace( gCWorkspace const & );
-    gCWorkspace( void );
+protected:
+    void Invalidate( void );
 };
 GE_ASSERT_SIZEOF( gCWorkspace, 0x001C );
 
