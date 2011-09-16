b3d5bc838ec38f0f46c3f7f89fbcd961e1d89638
nicode <nicode@1f35f6f1-ac98-46d3-9b0a-30c6b533b274>
2011-09-16 12:54:09 +0000

    Script PSParty and dependencies added.

---
+++ trunk/source/Game/ge_partyadmin.h
@@ -0,0 +1,30 @@
+#ifndef GE_PARTYADMIN_H_INCLUDED
+#define GE_PARTYADMIN_H_INCLUDED
+
+#pragma warning( push )
+#pragma warning( disable : 4251 )  // class 'bTValArray<T>' needs to have dll-interface to be used by clients of class 'gCPartyAdmin'
+
+class GE_DLLIMPORT gCPartyAdmin :
+    public eCEngineComponentBase
+{
+GE_DECLARE_PROPERTY_OBJECT( gCPartyAdmin, eCEngineComponentBase )
+public: virtual GEBool   IsValid( void ) const;
+public: virtual bEResult Create( void );
+public: virtual void     Destroy( void );
+public: virtual bEResult Initialize( void );
+public: virtual bEResult PostInitialize( void );
+public: virtual bEResult Shutdown( void );
+protected:
+    bTValArray< gCParty * > m_arrParties;
+public:
+    bCString  GetNewPartyName( gCParty_PS const * );
+    void      Invalidate( void );
+    GEBool    IsPartyNameValid( bCString const & );
+    gCParty * QueryParty( bCString const &, eCEntityProxy const & );
+    gCParty * ReleaseParty( bCString const &, eCEntityProxy const & );
+};
+GE_ASSERT_SIZEOF( gCPartyAdmin, 0x001C )
+
+#pragma warning( pop )
+
+#endif
